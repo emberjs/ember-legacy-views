1e8e3f557939a21abf29357ca2f4341b251e181e
Cory Forsyth <cory.forsyth@gmail.com>
2015-06-09 13:20:00 -0400

    Modify test to fail on raised deprecation

--- tests/unit/view-warning-is-silenced-test.js
+++ tests/unit/view-warning-is-silenced-test.js
@@ -6,6 +6,12 @@ module('Ember.View');
 
 test('doest not issue deprecation on create', function(assert){
   assert.expect(1);
-  Ember.View.create();
+
+  try {
+    Ember.View.create();
+  } catch(e) {
+    assert.ok(false, `An error was thrown unexpectedly: ${e.message}`);
+  }
+
   assert.ok(true, 'No deprecation is thrown');
 });

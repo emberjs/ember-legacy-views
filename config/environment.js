6297af64af76c7c44b426a8136192cf7ed9020f6
Cory Forsyth <cory.forsyth@gmail.com>
2015-06-09 15:57:04 -0400

    Use underscored env variable _ENABLE_LEGACY_VIEW_SUPPORT

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@
 module.exports = function(/* environment, appConfig */) {
   return {
     EmberENV: {
-      ENABLE_LEGACY_VIEW_SUPPORT: true
+      _ENABLE_LEGACY_VIEW_SUPPORT: true
     }
   };
 };
